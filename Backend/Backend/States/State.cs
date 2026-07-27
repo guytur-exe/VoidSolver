040207a3cf7f8928020002fef409fe986ff8a663
Guy Turgeman Dvir <guy11td@gmail.com>
2026-07-27 23:44:34 +0300

    skeleton files

--- Backend/Backend/States/State.cs
+++ Backend/Backend/States/State.cs
@@ -1,7 +1,9 @@
 ﻿using Backend.Board.Tiles;
 using Backend.Units;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +13,45 @@ namespace Backend.States
     internal class State
     {
         private ITile[][] tiles;
-        Player player;
+        private Player player;
+        private List<Monster>? monsters;
+        private List<Shade>? shades;
+        private List<Statue>? statues;
 
-        public State(Player p, )
+        private State? previus;
+        private char? input;
+        private bool visited;
+
+        public State(char inp, State? prev, Player p, List<Monster> m, List<Shade> sh, List<Statue> st)
+        {
+            input = inp;
+            previus = prev;
+            player = p;
+            monsters = m;
+            shades = sh;
+            statues = st;
+            Tick();
+        }
+
+        public State(State? prev, Player p, List<Monster> m, List<Shade> sh, List<Statue> st)
+        {
+            input = null;
+            previus = prev;
+            player = p;
+            monsters = m;
+            shades = sh;
+            statues = st;
+            Tick();
+        }
+
+        public State? Prev
+        {
+            get { return previus; }
+            set { previus = value; }
+        }
+
+        [MemberNotNull(nameof(tiles))]
+        public void Tick()
         {
 
         }
--- Backend/Backend/States/StateGraph.cs
+++ Backend/Backend/States/StateGraph.cs
@@ -8,6 +8,57 @@ namespace Backend.States
 {
     internal class StateGraph
     {
+        /// <summary>
+        /// keeps generating and saving states of the graph, 
+        /// graph relationships are ignored
+        /// </summary>
+        private State start;
+        private State end;
+        bool? solved = null;
+        private HashSet<State> states;
+        private Queue<State> queue;
+        public StateGraph(State s)
+        {
+            start = s;
+            queue = new Queue<State>();
+        }
+        /// <summary>
+        /// generates next states that can be derived from a certain state 
+        /// from going up, down, left or right, or when pressing z.
+        /// states that kill the player will be dropped
+        /// </summary>
+        /// <param name="s">current state to derive next states from</param>
+        public void GenerateNextStates(State s)
+        {
+
+        }
+        /// <summary>
+        /// evaluates a path from the state 'start' to a state where the level is solved.
+        /// if the level cannot be solved or the max path length is reached the search stops and doesn't find a path
+        /// </summary>
+        public void EvaluatePath()
+        {
+
+        }
+
+        public LinkedList<State>? Path
+        {
+            get 
+            {
+                if (solved ?? false)
+                {
+                    return null;
+                }
+                LinkedList<State> path = new LinkedList<State>();
+                State? pos = end;
+                while (pos != null)
+                {
+                    path.AddFirst(pos);
+                    pos = pos.Prev;
+                }
+                return path;
+            }
+        }
 
     }
 }
